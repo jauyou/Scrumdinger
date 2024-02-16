6c938573d98fc0aaa8e26dee9346c0afaada47bd
Chen Chao Yu <jauyou@gmail.com>
2024-02-16 21:02:47 +0800

    Complete Section 4: Supplement accessibility data

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Maxwell on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
